6f5344862b36becebd20076f753b671251be2b82
cyao1234 <chengyin.yao@live.com>
2023-02-16 21:48:23 +0100

    Dynamic alloc

--- src/main.c
+++ src/main.c
@@ -5,6 +5,8 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <stddef.h>
+
+#include <attributes.h>
 #include <kernel/ports.h>
 
 #define BLOCK_SIZE 4096
@@ -45,18 +47,18 @@ static inline int strcmp(const char*restrict lhs, const char*restrict rhs) {
 
 #define seek(addr) outd(SD_SEEK, addr)
 
-uint32_t inode_size;
-uint32_t inode_table_address;
+uint32_t* inode_size;
+uint32_t* inode_table_address;
 
 uint32_t get_dir_inode(uint32_t parent_inode, const char*restrict name) {
     uint8_t name_length;
     char name_buffer[256];
     uint32_t block_pointers[15];
 
-    seek(inode_table_address + ((parent_inode - 1) * inode_size) / BLOCK_SIZE);
+    seek((*inode_table_address) + ((parent_inode - 1) * (*inode_size)) / BLOCK_SIZE);
 
     for (int i = 0; i < 15; i++) 
-        block_pointers[i] = inw(SD_DATA + (parent_inode - 1) * inode_size + 40 + i * 4);
+        block_pointers[i] = inw(SD_DATA + (parent_inode - 1) * (*inode_size) + 40 + i * 4);
 
     for (int i = 0; i < 13; i++) {
         if (block_pointers[i] == 0)
@@ -88,50 +90,46 @@ uint32_t get_dir_inode(uint32_t parent_inode, const char*restrict name) {
     PANIC(" not found!");
 }
 
-void block_to_mems(uint64_t number, uint64_t block, uint64_t mem) {
-    for (int i = 0; i < number; i++) {
-        seek(block + i);
-        for (uint64_t j = 0; j < 0x1000; j += 4)
-            *((uint64_t *) mem + j + i * BLOCK_SIZE) = ind(SD_DATA + j);
-    }
-}
-
 uint32_t getblock(uint32_t inode, uint64_t number) {
     return ;
 }
 
 uintptr_t readelf(uint32_t inode) {
     // Here comes the tough stuff :(
-    seek(inode_table_address + ((inode - 1) * inode_size) / BLOCK_SIZE);
+    seek((*inode_table_address) + ((inode - 1) * (*inode_size)) / BLOCK_SIZE);
 
-    seek(inw(SD_DATA + (inode - 1) * inode_size + 40)); // Go to block 0 of the file
+    seek(inw(SD_DATA + (inode - 1) * (*inode_size) + 40)); // Go to block 0 of the file
 
     // Check for elf header  \/ = 0x7F E L F
     if (inw(SD_DATA) == 0x464C457F) {
         puts("kernel recognised");
 
-        uintptr_t entry = ind(0x18), phoff = ind(0x20);
-        uint16_t phnum = inh(0x38);
+        uintptr_t program_entry = ind(0x18), phoff = ind(0x20);
+        uint16_t entsize = inh(0x36), phnum = inh(0x38);
+        
+        pghr* hr = BUILTIN_ALLOC(phnum * entsize); // Alloc this on stack, which is effectivly at the end of memory
 
-        block_to_mem(phoff % BLOCK_SIZE, inq(MEM_SIZE));
+        
 
-        pghr hr = phoff / BLOCK_SIZE;
-
-        return entry;
+        return program_entry;
     } else /* if not elf */ {
         PANIC("kernel is not elf!");
     }
 }
 
 uintptr_t main(void) {
-    puts("Achieve BIOS (Pre-Alpha)");
+    // Use the stack to avoid global variables
+    inode_size = BUILTIN_ALLOC(sizeof(uint32_t));
+    inode_table_address = BUILTIN_ALLOC(sizeof(uint32_t));
+    
+    puts("Achieve BIOS (Pre-Alpha)\n\n");
 
     seek(0);
-    inode_size = inw(SD_DATA + 1024 + 88);
+    *inode_size = inw(SD_DATA + 1024 + 88);
 
     // Block group
     seek(1);
-    inode_table_address = inw(SD_DATA + 8);
+    *inode_table_address = inw(SD_DATA + 8);
 
     return readelf(get_dir_inode(get_dir_inode(get_dir_inode(get_dir_inode(2, "System"), "Library"), "Kernel"), "kernel"));
 }
